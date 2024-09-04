cbc9b8573ea19c475b719130246af24078a87a67
Patrick Driscoll <pdriscoll@satx.rr.com>
2024-09-04 16:52:44 -0500

    Finish initial all donations

--- src/timelines/all.py
+++ src/timelines/all.py
@@ -11,22 +11,46 @@ import pandas as pd
 
 def create_timeline(
     df,
-    output_dir,
+    output_file,
     date_start=DATA_START,
     date_end=DATA_END,
     year_cutoff=YEAR_CUTOFF
 ):
-    pass
+    """
+    Gets member donation data file and then:
+        -filters by needed fields, and date_start and date_end
+        -creates time frequency column
+        -aggregate donations on on ['time_frequency'] 
+        -saves csv file as output_file        
+
+    Args:
+        df (pandas.DataFrame):  dataframe of donations from donors working data in data/processed/.
+        output_file (str): path to where results are saved as a csv file.
+        date_start (str): for date range filter, is inclusive, in format 2019-10-01.
+        date_end (str): for date range filter, is inclusive, in format 2022-09-30.
+        year_cutoff (str): where to cutoff year timeframe, is inclusive, defaults to fy.                    
+    
+    References for @year_cutoff:
+      -https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html
+      -https://stackoverflow.com/questions/22205159/format-pandas-datatime-object-to-show-fiscal-years-from-feb-to-feb-and-be-format    
+    """  
+
+    df = df[(df['Date'] >= date_start) & (df['Date'] <= date_end)] #filter by date range    
+    df['Payments'] = df['Paid to Date'] + df['Balance'] #add paid and balance
+    df['Date'] = pd.to_datetime(df['Date']).dt.to_period(year_cutoff) #set years
+    df = df[['Date', 'Payments']] #keep only needed fields
+    df = df.rename(columns={'Date': 'Year', 'Payments': 'Donations'})
+
+    aggreg = {'Donations': 'sum'}
+    df = df.groupby('Year').agg(aggreg)
+    df.to_csv(output_file)
 
 def main():
     arg = parse_args()
     df = get_data() 
-    output_dir = get_output_dir('timelines')   
-
-    print('\nCHECKS:')
-    print('ARG:', arg)
-    print('DF HEAD:\n', df.head())
-    print('\nOUTPUT_DIR:', output_dir)
+    output_dir = get_output_dir('timelines') 
+    output_file = os.path.join(output_dir, f'all_{arg}.csv')  
+    create_timeline(df, output_file)
 
 if __name__ == '__main__':
     sys.exit(main())
